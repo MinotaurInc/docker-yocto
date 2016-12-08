74b4083fe54e87ba1ba9106eb0a60509a3675327
Derek Straka <derek@asterius.io>
2016-12-08 12:35:09 -0500

    Add some VAD specific env vars to the whitelist
    Signed-off-by: Derek Straka <derek@asterius.io>

--- bitbake.sh
+++ bitbake.sh
@@ -3,4 +3,4 @@
 cmd=$(basename $0)
 
 sudo -E /sbin/setuser builduser \
-	bash -c "source /var/build/poky/oe-init-build-env && export BB_ENV_EXTRAWHITE=\"${BB_ENV_EXTRAWHITE} MACHINE SSTATE_DIR SSTATE_MIRRORS SSH_AUTH_SOCK\" && ${cmd} $*"
+	bash -c "source /var/build/poky/oe-init-build-env && export BB_ENV_EXTRAWHITE=\"${BB_ENV_EXTRAWHITE} VAD_SCM_REPO VAD_SCM_PROTOCOL VAD_SCM_OBJ VAD_PRODUCT_VERSION MACHINE SSTATE_DIR SSTATE_MIRRORS SSH_AUTH_SOCK\" && ${cmd} $*"
